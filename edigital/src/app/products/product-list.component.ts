c836f981eadb819805322a1dedb9ba128c209b5f
elias2211 <32076201+elias2211@users.noreply.github.com>
2019-05-24 08:49:43 -0500

    Major change!

--- edigital/src/app/products/product-list.component.ts
+++ edigital/src/app/products/product-list.component.ts
@@ -6,4 +6,25 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent{
     
+    welcomeText: string = 'Welcome!';
+    products: any[] = [
+            
+    {
+        "productId": 2,
+        "productName": "Matrix",
+        "productCode": "GDN-002",
+        "releaseDate": "March 18, 2019",
+        "price": 39.99,
+        "productRating": 4.4
+    } ,  
+    {
+        "productId": 2,
+        "productName": "Game of thrones",
+        "productCode": "GDN-0098",
+        "releaseDate": "May 8, 2019",
+        "price": 89.99,
+        "productRating": 4.9
+    }  
+    
+  ];
 }
